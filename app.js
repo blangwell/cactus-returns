64425faf86d855fce604b199773060cfcb5eff8d
blangwell <b.langwell@outlook.com>
2020-12-27 19:32:08 -0800

    [rubberband] Attempt implementation of rubberband logic, clean up code

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const game = document.getElementById('game');
 const ctx = game.getContext('2d');
 
-let gameOver = true;
 let cactus;
+let gameOver = true;
 
 let keys = {
   ArrowUp: false,
@@ -24,7 +24,7 @@ function Character(x, y, color, width, height) {
   this.height = height;
   this.velX = 5;
   this.velY = 10;
-  this.maxX = game.width / 3;
+  this.maxX = game.width / 2;
   this.maxJump = this.y - 150;
   this.gravityRate = 4;
   this.jumping = false;
@@ -33,6 +33,13 @@ function Character(x, y, color, width, height) {
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.width, this.height)
   }
+};
+
+function rubberband() {
+  let slideBack = setInterval(() => {
+    if (!cactus.jumping && !keys.ArrowRight && !cactus.sliding) cactus.x -= 1;
+    if (cactus.x <= 75) clearInterval(slideBack)
+  }, 500)
 };
 
 function menu() {
@@ -44,51 +51,36 @@ function detectCollision(obj) {
 };
 
 function jump() {
-  // TODO prevent infinite jumping while holding down jump button
   // adding the check for !cactus.jumping prevents stutter jump
   if (!cactus.sliding && !cactus.jumping) {
     cactus.jumping = true;
-    // while (cactus.y >= cactus.maxJump) {
-    //   cactus.y -= 1;
-    // }
-
     let jumpAnimation = setInterval(() => {
-      if (cactus.y > cactus.maxJump) {
-        cactus.y -= cactus.velY;
-      } else {
-        clearInterval(jumpAnimation)
-      }
+      if (cactus.y > cactus.maxJump) cactus.y -= cactus.velY;
+      else clearInterval(jumpAnimation);
     }, 12)
   }
 };
 
 function slide() {
-  // TODO devise lateral movement logic
-  // move forward slightly, then 
-  // update() will pull back towards initial location
   if (!cactus.sliding && !cactus.jumping) {
     cactus.sliding = true;
-    cactus.y += cactus.height / 2;
+    cactus.y += Math.min(cactus.height / 2);
     [cactus.width, cactus.height] = [cactus.height, cactus.width];
+    let slideDistance = cactus.x + cactus.width;
     let slideForward = setInterval(() => {
-      if (cactus.x < cactus.maxX) cactus.x += 5;
-    }, 12)
+      if (cactus.x <= slideDistance 
+        && cactus.x <= cactus.maxX) {
+          cactus.x += 5;
+        } 
+    }, 12);
 
     setTimeout(() => {
+      clearInterval(slideForward)
       cactus.sliding = false;
       cactus.y -= cactus.height;
       [cactus.height, cactus.width] = [cactus.width, cactus.height];
-      clearInterval(slideForward)
-    }, 600);
+    }, 500);
   } 
-};
-
-function keydownHandler(e) {
-  keys[e.code] = true;
-};
-
-function keyupHandler(e) {
-  keys[e.code] = false;
 };
 
 function update() {
@@ -96,11 +88,11 @@ function update() {
   if (keys.ArrowUp) {
     if (!cactus.jumping && !cactus.sliding) {
       jump();
-    }
+    } 
   }
   if (keys.ArrowDown) {
     slide(); 
-  }
+  } 
   if (keys.ArrowRight) {
     // TODO if moving right, speed up the scroll of background/enemies
     if (cactus.x < cactus.maxX) {
@@ -114,28 +106,23 @@ function update() {
     }
   }
   // detect floor
-  if (cactus.y + cactus.height >= game.height) {
-    cactus.jumping = false;
-  } 
+  if (cactus.y + cactus.height >= game.height) cactus.jumping = false;
   // apply gravity if jumping
   if (cactus.jumping) cactus.y += cactus.gravityRate;
+};
 
-  // slide back towards center of movement area
-  if (!keys.ArrowRight && !cactus.sliding) {
-    setTimeout(() => {
-      let slideBack = setInterval(() => {
-        if (cactus.x > 50 && !cactus.sliding && !cactus.jumping) cactus.x -= 1;
-        else clearInterval(slideBack)
-      }, 100)
-    }, 1000)
-  }
-  
+function keydownHandler(e) {
+  keys[e.code] = true;
+};
+
+function keyupHandler(e) {
+  keys[e.code] = false;
 };
 
 function gameLoop() {
   ctx.clearRect(0, 0, game.width, game.height);
-  cactus.draw();
   update();
+  cactus.draw();
   window.requestAnimationFrame(gameLoop);
 };
 
